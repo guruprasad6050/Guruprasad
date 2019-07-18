4ba1b0e132ac18cffd69c5c71ca9498de81c4742
guruprasad6050 <53034240+guruprasad6050@users.noreply.github.com>
2019-07-18 12:17:48 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 a=int(input("type the number: "))
 if a=>1:
-    print("It is Positive")
+    print("Positive")
 elif a==0:
-    print("It is zero ")
+    print("zero")
 else :
-    print("It is negative")
+    print("negative")
